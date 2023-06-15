490ee08e11400fa6481c21f37dacd155f13886a7
Gnarll <vadimfedorovich22@mail.ru>
2023-06-15 19:54:30 +0300

    add routing

--- server/index.js
+++ server/index.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
-require("dotenv").config();
 const mongoose = require("mongoose");
+const router = require("./router/index");
+require("dotenv").config();
 
 const PORT = process.env.PORT || 5000;
 const app = express();
@@ -10,6 +11,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors());
+app.use("/api", router);
 
 const start = async () => {
   try {
