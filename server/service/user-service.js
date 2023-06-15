7420678e24bc4f8d121b2cbf9614ea051fce9476
Gnarll <vadimfedorovich22@mail.ru>
2023-06-15 21:58:17 +0300

    implement registration logic

---
+++ server/service/user-service.js
@@ -0,0 +1,33 @@
+const uuid = require("uuid");
+const UserModel = require("../models/user-model");
+const mailService = require("./mail-service");
+const bcrypt = require("bcrypt");
+const tokenService = require("./token-service");
+const UserDto = require("../dtos/user-dto");
+
+class UserService {
+  async registration(email, password) {
+    const candidate = await UserModel.findOne({ email });
+    if (candidate)
+      throw new Error(`Пользователь с адресом ${email} уже существует`);
+
+    const hashedPassword = await bcrypt.hash(password, 3);
+    const activationLink = uuid.v4();
+
+    const user = await UserModel.create({
+      email,
+      password: hashedPassword,
+      activationLink,
+    });
+
+    await mailService.sendActivationMail(email, activationLink);
+
+    const userDto = new UserDto(user);
+    const tokens = tokenService.generateTokens({ ...userDto });
+    await tokenService.saveToken(userDto.id, tokens.refreshToken);
+
+    return { ...tokens, user: userDto };
+  }
+}
+
+module.exports = new UserService();
