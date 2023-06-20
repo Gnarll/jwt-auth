57deb573efdd51efacc452e653e72ab49408210e
Gnarll <vadimfedorovich22@mail.ru>
2023-06-20 20:39:58 +0300

    validate registration fields

--- server/controllers/user-controller.js
+++ server/controllers/user-controller.js
@@ -1,9 +1,17 @@
 const userService = require("../service/user-service");
+const { validationResult } = require("express-validator");
+const ApiError = require("../exceptions/api-error");
 
 class UserController {
   async registration(req, res, next) {
     try {
       const { email, password } = req.body;
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return next(
+          ApiError.BadRequest("Ошибка при валидации", errors.array())
+        );
+      }
       const userData = await userService.registration(email, password);
       res.cookie("refreshToken", userData.refreshToken, {
         maxAge: 30 * 24 * 60 * 60 * 1000,
