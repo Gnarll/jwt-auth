8300858fbb236a9d3400d54b31cf36efe74d72cc
Gnarll <vadimfedorovich22@mail.ru>
2023-06-26 19:51:11 +0300

    add store and fix cors

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import { LoginForm } from "./components/LoginForm";
 
 const App: React.FC = () => {
-  return <div>Hello, world!</div>;
+  return (
+    <div>
+      <LoginForm />
+    </div>
+  );
 };
 
 export default App;
---
+++ client/src/components/LoginForm.tsx
@@ -0,0 +1,32 @@
+import React, { FC, useContext, useState } from "react";
+import { Context } from "../index";
+
+export const LoginForm: FC = () => {
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const { store } = useContext(Context);
+  return (
+    <div>
+      <input
+        onChange={(e) => {
+          setEmail(e.target.value);
+        }}
+        type="text"
+        placeholder="Email"
+        value={email}
+      />
+      <input
+        onChange={(e) => {
+          setPassword(e.target.value);
+        }}
+        type="password"
+        placeholder="Password"
+        value={password}
+      />
+      <button onClick={() => store.login(email, password)}>Логин</button>
+      <button onClick={() => store.registration(email, password)}>
+        Регистрация
+      </button>
+    </div>
+  );
+};
